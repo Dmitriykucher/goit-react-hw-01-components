148e1f0eac23352c6a2b5f46ce246472b9a695db
Dmitriy Kucher <dmitriykucher111@gmail.com>
2019-10-17 00:39:01 +0300

    Updates

---
+++ precache-manifest.01dab4089305d5c3537337996316161a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad965a260ae2455925b6d25d0c708d9a",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "3a49609b5b780631e17b",
+    "url": "/goit-react-hw-01-components/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9f8bf027803cc60900c5",
+    "url": "/goit-react-hw-01-components/static/js/2.7ae55013.chunk.js"
+  },
+  {
+    "revision": "3a49609b5b780631e17b",
+    "url": "/goit-react-hw-01-components/static/js/main.0a74ed74.chunk.js"
+  },
+  {
+    "revision": "1deac508ae55ff559187",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.a749bb14.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/goit-react-hw-01-components/static/media/logo.25bf045c.svg"
+  }
+]);
